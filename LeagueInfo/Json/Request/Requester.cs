b2944328a60e05aa185b0e7c42cb2a2cd9ed9470
klausner <klausner17@gmail.com>
2015-07-29 00:30:36 -0300

    Fazendo testes na requisição do JSON

--- LeagueInfo/Json/Request/Requester.cs
+++ LeagueInfo/Json/Request/Requester.cs
@@ -16,65 +16,23 @@ namespace LeagueInfo.Json.Request
         const string VERSION = "v1.2";
         const string KEY = @"8eee2093-91d0-4a8f-bc85-c366e7de1c33";
         public string Campo { get; set; }
-        private static ManualResetEvent allDone = new ManualResetEvent(false);
+        private WebRequest requester;
 
-        public void ExecuteRequest()
+        private void StartWebRequest()
         {
-            // Create a new HttpWebRequest object.
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.contoso.com/example.aspx");
-
-            request.ContentType = "application/x-www-form-urlencoded";
-
-            // Set the Method property to 'POST' to post data to the URI.
-            request.Method = "POST";
-
-            // start the asynchronous operation
-            request.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), request);
-
-            // Keep the main thread from continuing while the asynchronous 
-            // operation completes. A real world application 
-            // could do something useful such as updating its user interface. 
-            allDone.WaitOne();
+            requester = WebRequest.Create(URLAPI);
+            requester.BeginGetResponse(new AsyncCallback(FinishWebRequest), null);
+            StreamReader stream = (StreamReader) requester.BeginGetRequestStream(new AsyncCallback(BeginGetStream), null);
         }
 
-        private void GetRequestStreamCallback(IAsyncResult ar)
+        private void BeginGetStream(IAsyncResult ar)
         {
-            HttpWebRequest request = (HttpWebRequest)ar.AsyncState;
-
-            // End the operation
-            Stream postStream = request.EndGetRequestStream(ar);
-
-            Console.WriteLine("Please enter the input data to be posted:");
-            string postData = Console.ReadLine();
-
-            // Convert the string into a byte array. 
-            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-
-            // Write to the request stream.
-            postStream.Write(byteArray, 0, postData.Length);
-            postStream.Close();
-
-            // Start the asynchronous operation to get the response
-            request.BeginGetResponse(new AsyncCallback(GetResponseCallback), request);
+            requester.EndGetResponse(ar);
         }
 
-        private void GetResponseCallback(IAsyncResult ar)
+        private void FinishWebRequest(IAsyncResult result)
         {
-            HttpWebRequest request = (HttpWebRequest)ar.AsyncState;
-
-            // End the operation
-            HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(ar);
-            Stream streamResponse = response.GetResponseStream();
-            StreamReader streamRead = new StreamReader(streamResponse);
-            string responseString = streamRead.ReadToEnd();
-            Console.WriteLine(responseString);
-            // Close the stream object
-            streamResponse.Close();
-            streamRead.Close();
-
-            // Release the HttpWebResponse
-            response.Close();
-            allDone.Set();
+            requester.EndGetResponse(result);
         }
     }
 }
