47e337e14a7974258da3a6b689d78bc124d57849
klausner <klausner17@gmail.com>
2015-07-29 17:10:25 -0300

    Adicionando ItemSelect e TODOLIST

---
+++ LeagueInfo/Controls/ItemSelect.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace LeagueInfo
+{
+    public partial class ItemSelect : UserControl
+    {
+
+        public Image Icon
+        {
+            get { return this.icon; }
+            set { this.icon = value; }
+        }
+
+        public String Title
+        {
+            get { return this.title.Text; }
+            set { this.title.Text = value; }
+        }
+
+        public String Description 
+        { 
+            get { return this.description.Text; } 
+            set { this.description.Text = value; } 
+        }
+
+        public ItemSelect()
+        {
+            InitializeComponent();
+        }
+    }
+}
