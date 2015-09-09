dbb7375c74b80126c9ae8d17315c93878480ba07
klausner <klausner17@gmail.com>
2015-09-09 08:17:29 -0300

    Adicionando classe das habilidades

---
+++ LeagueInfo/Json/ChampionSpellDto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LeagueInfo.Json
+{
+    /// <summary>
+    /// Esta classe representa as habilidades do campeão.
+    /// </summary>
+    public class ChampionSpellDto
+    {
+        [JsonProperty("altimages")]
+        public List<ImageDto> AltImages { get; set; }
+
+        [JsonProperty("cooldown")]
+        public List<double> Cooldown { get; set; }
+
+        [JsonProperty("cooldownBurn")]
+        public string CooldownBurn { get; set; }
+
+        [JsonProperty("cost")]
+        public List<int> Cost { get; set; }
+
+        [JsonProperty("costBurn")]
+        public string CostBurn { get; set; }
+
+        [JsonProperty("costType")]
+        public string CostType { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("effect")]
+        public List<List<double>> Effect { get; set; }
+
+        [JsonProperty("effectBurn")]
+        public List<string> EffectBurn { get; set; }
+
+        [JsonProperty("image")]
+        public ImageDto Image { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("leveltip")]
+        public LevelTipDto LevelTip { get; set; }
+
+        [JsonProperty("maxrank")]
+        public int MaxRank { get; set; }
+
+        //[JsonProperty("range")]
+        //public object Range { get; set; }
+
+        [JsonProperty("rangeBurn")]
+        public string RangeBurn { get; set; }
+
+        [JsonProperty("resource")]
+        public string Resource { get; set; }
+
+        [JsonProperty("sanitizedDescription")]
+        public string SanitizedDescription { get; set; }
+
+        [JsonProperty("tooltip")]
+        public string ToolTip { get; set; }
+
+        //[JsonProperty("vars")]
+        //public List<SpellVarsData> Vars { get; set; }
+    }
+}
