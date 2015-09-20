de40a7ed7e5e04bce224c12ecc544feb55a24fe1
klausner <klausner17@gmail.com>
2015-09-20 13:07:26 -0300

    Colocando campões por ordem alfabética.

--- LeagueInfo/Json/ChampionListDto.cs
+++ LeagueInfo/Json/ChampionListDto.cs
@@ -9,10 +9,9 @@ using System.IO;
 
 namespace LeagueInfo.ClassApi
 {
-    class ChampionListDto
+    class ChampionListDto : IDisposable
     {
         const String URLALLCHAMPS = @"https://global.api.pvp.net/api/lol/static-data/br/v1.2/champion?champData=image,tags&api_key=8eee2093-91d0-4a8f-bc85-c366e7de1c33";
-        public static ChampionListDto AllChampions;
 
         [JsonProperty("data")]
         public Dictionary<string, ChampionDto> Data { get; set; }
@@ -29,13 +28,30 @@ namespace LeagueInfo.ClassApi
         [JsonProperty("version")]
         public string Version { get; set; }
 
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+
         public async Task<ChampionListDto> LoadAllChampions()
         {
             string json = await new Requester(URLALLCHAMPS).GetJson();
             if (json == string.Empty)
                 json = new StreamReader(@"Assets\Cache\jsonchampions.txt", UnicodeEncoding.UTF8).ReadLine();
-            AllChampions = new ChampionListDto();
-            return AllChampions = JsonConvert.DeserializeObject<ChampionListDto>(json);
+            ChampionListDto allChampions = new ChampionListDto();
+            allChampions =  JsonConvert.DeserializeObject<ChampionListDto>(json);
+            var championsByName = from championData in allChampions.Data
+                                  orderby championData.Value.Name ascending
+                                  select championData;
+            ChampionListDto championsByOrderName = new ChampionListDto();
+            championsByOrderName.Data = new Dictionary<string, ChampionDto>();
+            int count = championsByName.Count();
+            foreach (KeyValuePair<string, ChampionDto> champ in championsByName)
+            {
+                championsByOrderName.Data.Add(champ.Key, champ.Value);
+            }
+            allChampions.Dispose();
+            return championsByOrderName;
         }
     }
 }
