e925be38a99dbf5cc77046e422f23383ead92149
klausner <klausner17@gmail.com>
2015-11-19 19:15:39 -0200

    Iniciando a utilização de webservice, botão de login ja funciona

--- LeagueInfo/Pages/Login.xaml.cs
+++ LeagueInfo/Pages/Login.xaml.cs
@@ -19,7 +19,29 @@ namespace LeagueInfo.Pages
 
         private void buttonLogin_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Pages/MainPage.xaml", UriKind.RelativeOrAbsolute));
+            LeagueWS.LeagueServiceClient ls = new LeagueWS.LeagueServiceClient();
+            ls.loginCompleted += ls_loginCompleted;
+            ls.loginAsync(nomeInvocador.Text, senha.Password);
+        }
+
+        void ls_loginCompleted(object sender, LeagueWS.loginCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Result)
+                    NavigationService.Navigate(new Uri("/Pages/MainPage.xaml?logado=true", UriKind.RelativeOrAbsolute));
+                else
+                    MessageBox.Show("Login não efetuado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message);
+            }
+        }
+
+        private void HyperlinkButton_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            e.ca
         }
     }
 }
